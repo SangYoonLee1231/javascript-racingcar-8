601802ac523520def7e4f1b3aba389fc37d2fd9e
SangYoonLee1231 <sylagape1231@gmail.com>
2025-10-27 21:01:46 +0900

    refactor: 에러 처리 코드 수정 및 재작성

--- src/views/inputView.js
+++ src/views/inputView.js
@@ -2,14 +2,15 @@ import { MissionUtils } from "@woowacourse/mission-utils";
 
 const inputView = {
   async readCarNames() {
-    try {
-      const input = await MissionUtils.Console.readLineAsync(
-        "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
-      );
-      return input;
-    } catch (error) {
-      throw new Error("[ERROR] 자동차 이름을 다시 입력해주세요.");
+    const input = await MissionUtils.Console.readLineAsync(
+      "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
+    );
+
+    if (!inputStr || inputStr !== "string") {
+      throw new Error("입력값이 올바르지 않습니다.");
     }
+
+    return input;
   },
 };
 
