ca3f7d80bbac7b1df978154b891ba1eaf74db6e1
SangYoonLee1231 <sylagape1231@gmail.com>
2025-10-27 21:05:27 +0900

    feat: App.js에 자동차 이름 입력 받고 이를  파싱하는 함수를 호출하여 기능이 동작하도록 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import inputView from "./views/inputView";
+
 class App {
-  async run() {}
+  async run() {
+    try {
+      // 자동차 이름 입력 받기
+      const rawInput = await inputView.readCarNames();
+
+      // 자동차 이름 파싱
+      const carNamesArray = parseCarNames(rawInput);
+    } catch (error) {
+      throw new Error("[ERROR] " + error.message);
+    }
+  }
 }
 
 export default App;
